641d4848aae5ab990696dfc81683d372459dd436
Nick Papayiannakis <nickpapayiannakis@Nicks-iMac.local>
2022-01-04 10:46:37 -0500

    Added genesis block and typed useful functions

--- minimal_blockchain.hs
+++ minimal_blockchain.hs
@@ -1,14 +1,34 @@
 module MinimalBlockchain where
 
+import Data.Time.Clock.POSIX
+
 data Transaction = Transaction { from  :: String
                                , to    :: String
-                               , value :: Float } deriving Show
+                               , value :: Float 
+                               } deriving Show
 
-data Block = Block { version        :: String
+data Block = Block { version        :: Int
                    , index          :: Int
                    , transactions   :: [Transaction]
                    , prevBlockHash  :: String
                    , merkleRootHash :: String
-                   , time           :: ClockTime
-                   , nonce          :: Integer } deriving Show
+                   , time           :: POSIXTime
+                   , nonce          :: Maybe Integer 
+                   } deriving Show
 
+genesis :: IO Block
+genesis = do
+    let version = 1
+    let  index = 0
+    let  transactions = []
+    let  prevBlockHash = "000000000000000000000000000000000"
+    let  merkleRootHash = ""
+    time <- getPOSIXTime
+    let  nonce = Nothing
+    return (Block version index transactions prevBlockHash merkleRootHash time Nothing)
+
+addTransaction :: Transaction -> [Transaction]
+addTransaction = undefined
+
+hashBlock :: Block -> String
+hashBlock = undefined
