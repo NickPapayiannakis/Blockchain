4c19c92f37dfe094cace383391c5062fe9039c1c
Nick Papayiannakis <nickpapayiannakis@Nicks-iMac.local>
2022-01-04 23:00:01 -0500

    Add block hashing function

--- minimal_blockchain.hs
+++ minimal_blockchain.hs
@@ -1,7 +1,13 @@
 module MinimalBlockchain where
 
+
+import qualified Data.ByteString.Char8 as Char8
+import qualified Crypto.Hash.SHA256 as SHA256
 import Data.Time.Clock.POSIX
 
+---------------
+--Definitions--
+---------------
 data Transaction = Transaction { from  :: String
                                , to    :: String
                                , value :: Float } deriving Show
@@ -25,8 +31,21 @@ genesis = do
     let nonce = Nothing
     return (Block version index transactions prevBlockHash merkleRootHash time nonce)
 
+--------------------
+--Blockchain tools--
+--------------------
+
 addTransaction :: Block -> Transaction -> Block
 addTransaction (Block v i txs ph mh t n) tx = Block v i (txs ++ [tx]) ph mh t n 
 
+----------------
+--Cryptography--
+----------------
+
 hashBlock :: Block -> String
-hashBlock = undefined
+hashBlock (Block v i txs ph mh t n) = digest
+  where
+    digest = Char8.unpack (SHA256.finalize ctx)
+    ctx    = SHA256.updates ctx0 $ fmap Char8.pack [blockTxStr, ph]
+    ctx0   = SHA256.init
+    blockTxStr = foldr ((++) . show) "" txs
