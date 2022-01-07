23f88d6a14da5c59bd076267ea5a14da8fb4399b
Nick Papayiannakis <nickpapayiannakis@Nicks-iMac.local>
2022-01-07 11:52:14 -0500

    Add mining function, remover POISIX time to eliminate type issues

--- minimal_blockchain.hs
+++ minimal_blockchain.hs
@@ -4,6 +4,7 @@ module MinimalBlockchain where
 import qualified Data.ByteString.Char8 as Char8
 import qualified Crypto.Hash.SHA256 as SHA256
 import Data.Time.Clock.POSIX
+import Data.String   (fromString)
 
 ---------------
 --Definitions--
@@ -17,35 +18,40 @@ data Block = Block { version        :: Int
                    , transactions   :: [Transaction]
                    , prevBlockHash  :: String
                    , merkleRootHash :: String
-                   , time           :: POSIXTime
-                   , nonce          :: Maybe Integer } deriving Show
+                   , nonce          :: Integer } deriving Show
 
-genesis :: IO Block
-genesis = do
-    let version = 1
-    let index = 0
-    let transactions = []
-    let prevBlockHash = "000000000000000000000000000000000"
-    let merkleRootHash = "000000000000000000000000000000000"
-    time <- getPOSIXTime
-    let nonce = Nothing
-    return (Block version index transactions prevBlockHash merkleRootHash time nonce)
+genesis :: Block
+genesis = Block
+    { version        = 1
+    , index          = 0
+    , transactions   = []
+    , prevBlockHash  = "000000000000000000000000000000000"
+    , merkleRootHash = "000000000000000000000000000000000"
+    , nonce          = 0 }
 
 --------------------
 --Blockchain tools--
 --------------------
 
 addTransaction :: Block -> Transaction -> Block
-addTransaction (Block v i txs ph mh t n) tx = Block v i (txs ++ [tx]) ph mh t n 
+addTransaction (Block v i txs ph mh n) tx = Block v i (txs ++ [tx]) ph mh n 
+
+mine :: Block -> Integer -> Block
+mine block@(Block v i txs ph _ _ ) n = case head pow of
+                        '0' -> Block v (i + 1) txs ph hashOfBlock n
+                        _   -> mine block (n + 1)
+  where
+    hashOfBlock = hashBlock block
+    ctx         = SHA256.updates SHA256.init $ fmap fromString [hashOfBlock, ph, show n]
+    pow         = Char8.unpack $ SHA256.finalize ctx
 
 ----------------
 --Cryptography--
 ----------------
 
 hashBlock :: Block -> String
-hashBlock (Block v i txs ph mh t n) = digest
+hashBlock (Block v i txs ph mh n) = digest
   where
-    digest = Char8.unpack (SHA256.finalize ctx)
-    ctx    = SHA256.updates ctx0 $ fmap Char8.pack [blockTxStr, ph]
-    ctx0   = SHA256.init
+    digest     = Char8.unpack (SHA256.finalize ctx)
+    ctx        = SHA256.updates SHA256.init $ map Char8.pack [blockTxStr, ph]
     blockTxStr = foldr ((++) . show) "" txs
